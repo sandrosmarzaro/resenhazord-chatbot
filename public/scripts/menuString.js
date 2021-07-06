50c361873fb15f409a8d2a7163086f4997af0dc3
Sandro <sansmarzaro@gmail.com>
2021-07-06 12:05:56 -0300

    update options menu

--- public/scripts/menuString.js
+++ public/scripts/menuString.js
@@ -3,6 +3,9 @@ module.exports = `\t\t*_MENU DE COMANDOS_*
 ,oi
 _- Mande um oi pro Resenhazord_
 
+,mangos
+_- Descubra se Mangos II vai nascer_
+
 ,mateus
 _- Xingue o Mateus_
 
@@ -15,9 +18,12 @@ _- Adiciona ou indica alguém aleatório_
 ,ban
 _- Bane ou Indica alguém aleatório_
 
+,stic
+_- Faça uma figurinha sem corte com a imagem na mensagem_
+
+,sticrop
+_- Faça uma figurinha com corte com a imagem na mensagem_
+
 ,img
 _- Receba uma imagem aleatória SFW_
-
-,mangos
-_- Descubra se Mangos II vai nascer_
 `
