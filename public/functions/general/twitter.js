c718422cfba876ede8c94cad41e5d4873a907ee5
Sandro Smarzaro <sansmarzaro@gmail.com>
2021-11-17 22:54:31 -0300

    [UPDATE] Twitter function

--- public/functions/general/twitter.js
+++ public/functions/general/twitter.js
@@ -3,31 +3,30 @@ module.exports = async function (client, message, command) {
     const link = command.substring(5, command.length);
 
     videoUrlLink.twitter.getInfo(link, {}, async (error, info) => {
-        if (error) {
-            console.error(error);
-        }
+        if (error) console.error(error);
         else {
             let linkMp4;
             let birateBigger;
             let firstMp4 = 0;
-            info.variants.forEach((element) => {
+            const infoVariants = await info.variants;
+            infoVariants.forEach((element) => {
                 let contenType = element.content_type;
-                let turnbirate = element.bitrate;
+                let turnBitrate = element.bitrate;
                 if ( (firstMp4 === 0) && (contenType === "video/mp4")) {
                     firstMp4++;
-                    birateBigger = turnbirate;
+                    birateBigger = turnBitrate;
                     linkMp4 = element.url;
                 }
-                else if (turnbirate > birateBigger && contenType === "video/mp4") {
+                else if (turnBitrate > birateBigger && contenType === "video/mp4") {
                     birateBigger = element.bitrate;
                     linkMp4 = element.url;
                 }
             });
             await client.sendFile(
-                message.chatId, 
+                await message.chatId, 
                 linkMp4, 
                 `Twitter Video`, 
-                `${info.full_text}`
+                `${infoVariants.full_text}`
             );
         }
     });
