b66082aff247b9141a5edd293dcfa9e105cbee6c
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-03-18 13:13:49 -0300

    [ADDED] New phones in Brawl function

--- public/functions/resenha/atives/resenhaBrawl.js
+++ public/functions/resenha/atives/resenhaBrawl.js
@@ -1,7 +1,7 @@
 module.exports = async function(client, message, command) {
     const brawlsenhistas = ["5528999223882", "5528988038529", "5528999443183",
     "5528999210796", "5528999292286", "5512981672239",
-    "5528999969123", "5528999670808"];
+    "5528999969123", "5528999670808", "5528992981473", "5528999295771"];
     
     let contacts = "";
     const text = command.substring(7, command.length);
