aeeb97c39d3d15372bdc6bbae9396ee5a73ff892
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-08-23 20:22:23 -0300

    [UPDATE] removing contacts in group commands

--- public/functions/resenha/resenhaLol.js
+++ public/functions/resenha/resenhaLol.js
@@ -1,7 +1,7 @@
 module.exports = async function(client, message, command) {
     const lolzeiros = ["5528999443183", "5528988038529", "5527999236797",
     "5528992570457", "5528999292286", "5528999292778",
-    "5528999498472", "5528992751401", "5528999295771", "5528999688536"];
+    "5528999498472", "5528992751401", "5528999688536"];
     
     let contacts = "";
     const text = command.substring(5, command.length);
--- public/functions/resenha/resenhaVal.js
+++ public/functions/resenha/resenhaVal.js
@@ -1,7 +1,7 @@
 module.exports = async function(client, message, command) {
     const valorosos = ["5528999223882", "5528988038529", "5527999236797",
     "5528992570457", "5528999292286", "5528999292778",
-    "5528999566204", "5528992981473", "5528999295771"];
+    "5528999566204", "5528992981473"];
     
     let contacts = "";
     const text = command.substring(5, command.length);
